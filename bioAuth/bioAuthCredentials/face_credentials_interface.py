a4ec3b001705ec0bd184fa23f4e041f53364da59
David Franklin <dave5801@gmail.com>
2018-02-03 10:19:14 -0800

    creating new class for face credentials, but on list of photos yet

--- bioAuth/bioAuthCredentials/face_credentials_interface.py
+++ bioAuth/bioAuthCredentials/face_credentials_interface.py
@@ -1,9 +1,36 @@
 import boto3
+import os
+from os import listdir
+from os.path import isfile, join
 
 #BUCKET = "amazon-rekognition"
 BUCKET = "hackthenowprojectbucket"
 KEY = "test.jpg"
 
+
+class FaceCredentialsAWSInterface(object):
+    def __init__(self, url_where_photos_are=None):
+        self.url_where_photos_are = url_where_photos_are
+
+
+    def getListOfPhotoUrlsFromADirectoryofFaces(self):
+        """Get Directory of Faces."""
+        if not os.path.isdir(self.url_where_photos_are):
+            return "Invalid File Path"
+
+        list_of_photos_that_exist = [f for f in listdir(self.url_where_photos_are) if isfile(join(self.url_where_photos_are))]
+
+        return list_of_photos_that_exist
+
+
+if __name__ == '__main__':
+    face_credentials_interface = FaceCredentialsAWSInterface("static/test_photos_for_checking_api/nicholas_cage/")
+
+    list_of_face_photos = face_credentials_interface.getListOfPhotoUrlsFromADirectoryofFaces()
+    print(list_of_face_photos)
+
+
+'''
 def detect_labels(bucket, key, max_labels=10, min_confidence=90, region="eu-west-1"):
     rekognition = boto3.client("rekognition", region)
     response = rekognition.detect_labels(
@@ -21,3 +48,4 @@ def detect_labels(bucket, key, max_labels=10, min_confidence=90, region="eu-west
 
 for label in detect_labels(BUCKET, KEY):
     print("{Name} - {Confidence}%".format(**label))
+    '''
