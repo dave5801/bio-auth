b1ce788ef3dfdf866163779cfd0e60bad29c8b2b
David Franklin <dave5801@gmail.com>
2018-02-03 16:32:06 -0800

    last commit of the day

--- bioAuth/bioAuthCredentials/create_and_populate_s3_buckets.py
+++ bioAuth/bioAuthCredentials/create_and_populate_s3_buckets.py
@@ -3,18 +3,26 @@ import os
 import boto.s3
 import sys
 from boto.s3.key import Key
+import string
+import random
+
+
+'''
+'''
+def generate_unique_s3_id(size=20, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-bucket_name = "placeholder"
+bucket_name = generate_unique_s3_id().lower() + "-bucket"
 conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
         AWS_SECRET_ACCESS_KEY)
 
 bucket = conn.create_bucket(bucket_name,
     location=boto.s3.connection.Location.DEFAULT)
 
-testfile = "cage1.png"
+testfile = "static/test_photos_for_checking_api/nicholas_cage/cage1.png"
 
 def percent_cb(complete, total):
     sys.stdout.write('.')
@@ -25,3 +33,4 @@ k = Key(bucket)
 k.key = 'cage1.png'
 k.set_contents_from_filename(testfile,
     cb=percent_cb, num_cb=10)
+
