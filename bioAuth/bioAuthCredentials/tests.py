b3de948249b96b74e71fca574a806099a559d4a1
David Franklin <dave5801@gmail.com>
2018-02-13 15:33:29 -0800

    one working test case, that's still wrong

--- bioAuth/bioAuthCredentials/tests.py
+++ bioAuth/bioAuthCredentials/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestS3BucketProperties(TestCase):
+
+    #def get_list_of_photos_from_local_directory(self):
+    #def get_aws_credentials
+    #generate_unique_s3_bucket_name
+    def setUp(self):
+        testfile = "static/test_photos_for_checking_api/nicholas_cage"
+
+        test_s3_props = S3BucketProperties(testfile)
+
+    def test_get_list_of_photos_from_local_directory(self):
+
+        self.assertEqual(True, False)
