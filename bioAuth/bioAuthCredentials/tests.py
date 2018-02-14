e216ccbfb67eaa52caa54b14de3f4f1673955210
David Franklin <dave5801@gmail.com>
2018-02-13 16:15:02 -0800

    finally a successful test case

--- bioAuth/bioAuthCredentials/tests.py
+++ bioAuth/bioAuthCredentials/tests.py
@@ -6,11 +6,16 @@ class TestS3BucketProperties(TestCase):
     #def get_list_of_photos_from_local_directory(self):
     #def get_aws_credentials
     #generate_unique_s3_bucket_name
-    def setUp(self):
-        testfile = "static/test_photos_for_checking_api/nicholas_cage"
+   # def setUp(self):
+    #    testfileurl = "static/test_photos_for_checking_api/nicholas_cage"
 
-        test_s3_props = S3BucketProperties(testfile)
+     #   test_s3_props = S3BucketProperties(testfileurl)
 
-    def test_get_list_of_photos_from_local_directory(self):
+    def test_s3BucketProperties_url_exists(self):
 
-        self.assertEqual(True, True)
+        testfileurl = "static/test_photos_for_checking_api/nicholas_cage"
+        test_s3_props = S3BucketProperties(testfileurl)
+
+        test_url_from_SS3BucketProperties = test_s3_props.url
+
+        self.assertEqual(testfileurl, test_url_from_SS3BucketProperties)
