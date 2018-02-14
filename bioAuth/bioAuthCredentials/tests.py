b055c4015996472bab7b24596c7d019ea91824b2
David Franklin <dave5801@gmail.com>
2018-02-13 16:22:00 -0800

    setting up test cases for remaining properties

--- bioAuth/bioAuthCredentials/tests.py
+++ bioAuth/bioAuthCredentials/tests.py
@@ -3,14 +3,6 @@ from bioAuthCredentials.create_and_populate_s3_buckets import S3BucketProperties
 # Create your tests here.
 class TestS3BucketProperties(TestCase):
 
-    #def get_list_of_photos_from_local_directory(self):
-    #def get_aws_credentials
-    #generate_unique_s3_bucket_name
-   # def setUp(self):
-    #    testfileurl = "static/test_photos_for_checking_api/nicholas_cage"
-
-     #   test_s3_props = S3BucketProperties(testfileurl)
-
     def test_s3BucketProperties_url_exists(self):
 
         testfileurl = "static/test_photos_for_checking_api/nicholas_cage"
@@ -19,3 +11,14 @@ class TestS3BucketProperties(TestCase):
         test_url_from_SS3BucketProperties = test_s3_props.url
 
         self.assertEqual(testfileurl, test_url_from_SS3BucketProperties)
+
+    def test_get_list_of_photos_from_local_directory(self):
+        self.assertEqual(True,True)
+
+    def test_get_aws_credentials(self):
+        self.assertEqual(True,True)
+    
+    def test_generate_unique_s3_bucket_name(self):
+        self.assertEqual(True,True)
+
+
