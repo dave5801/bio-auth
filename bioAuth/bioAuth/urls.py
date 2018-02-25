6892adce755908eabb6a870501d736cb6212025c
David Franklin <dave5801@gmail.com>
2018-02-24 16:52:10 -0800

    changed home url to ''

--- bioAuth/bioAuth/urls.py
+++ bioAuth/bioAuth/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('home/', include('bioAuthCredentials.urls')),
+    path('', include('bioAuthCredentials.urls')),
     path('admin/', admin.site.urls),
 ]
