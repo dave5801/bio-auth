9e7773e397cafb82d4873610a30ded618832b6d8
David Franklin <dave5801@gmail.com>
2018-02-26 13:59:52 -0800

    removed even more corpse code from views

--- bioAuth/bioAuthCredentials/views.py
+++ bioAuth/bioAuthCredentials/views.py
@@ -1,21 +1,6 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    """Home page view."""
 
     template_name = 'bioAuthCredentials/home.html'
-
-'''
-def home_view(request):
-    template = loader.get_template("bioAuthCredentials/home.html")
-    
-    response_body = template.render()
-    return HttpResponse(response_body)
-    '''
-
-
-#def index(request):
- #   return HttpResponse("Hello, world. You're bio auth home page.")
