a3babaf05989802cbfafd45db0c046260f3278dd
David Franklin <dave5801@gmail.com>
2018-02-26 13:57:53 -0800

    removed corpse code from urls

--- bioAuth/bioAuthCredentials/urls.py
+++ bioAuth/bioAuthCredentials/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from . import views
 from bioAuthCredentials.views import HomePageView
-# url(r'^$', HomeView.as_view(), name='home'),
+
 urlpatterns = [
-    #path('', views.home_view, name='home_view'),
     path('', HomePageView.as_view(), name='home'),
-    #path('', views.index, name='index'),
 ]
