f926da37febd3e816a7b69940c62541042c843ad
David Franklin <dave5801@gmail.com>
2018-02-26 13:55:53 -0800

    home displays

--- bioAuth/bioAuthCredentials/urls.py
+++ bioAuth/bioAuthCredentials/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from bioAuthCredentials.views import HomePageView
+# url(r'^$', HomeView.as_view(), name='home'),
 urlpatterns = [
-    path('', views.home_view, name='home_view'),
-    #url(r'^$', HomeView.as_view(), name='home'),
+    #path('', views.home_view, name='home_view'),
+    path('', HomePageView.as_view(), name='home'),
     #path('', views.index, name='index'),
 ]
