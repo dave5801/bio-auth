6c369f0468f6944e774494d247f40c1c7805f7ae
David Franklin <dave5801@gmail.com>
2018-02-27 14:45:12 -0800

    creating new url for login

--- bioAuth/bioAuth/urls.py
+++ bioAuth/bioAuth/urls.py
@@ -4,9 +4,12 @@
 from django.contrib import admin
 from django.urls import path
 from bioAuth.views import HomePageView
+from bioAuthFaceVerificationLogin.views import LoginPageView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('login/', bioAuthFaceVerificationLogin.views, name='login'),
     path('admin/', admin.site.urls),
+
 ]
